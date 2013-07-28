2740688848c92d10a747c0219f3da18fdd7aa7bd
Negated <youhavethewrong@gmail.com>
2013-07-28 18:12:14 -0500

    added a few methods

--- src/main/groovy/info/youhavethewrong/tic/Tic.groovy
+++ src/main/groovy/info/youhavethewrong/tic/Tic.groovy
@@ -2,7 +2,21 @@ package info.youhavethewrong.tic;
 
 public class Tic {
 
-	public double getTransRotation(engineRotation, gearRatio) {
-		engineRotation/gearRatio
+	private static final BigDecimal MM_TO_INCHES = 0.039370
+
+	public double getGearedRotation(rotation, gearRatio) {
+		rotation/gearRatio
+	}
+
+	public double getWheelRotationFromEngineRotation(engineRotation, transRatio, axleRatio) {
+		engineRotation/transRatio/axleRatio
+	}
+
+	public double getLengthOfOneWheelRotation(sectionWidth, wheelSize) {
+		((sectionWidth*MM_TO_INCHES*2)+wheelSize)*Math.PI/12
+	}
+
+	public double getVehicleSpeed(engineRotation, gearRatio, finalRatio, sectionWidth, wheelSize) {
+		getWheelRotationFromEngineRotation(engineRotation, gearRatio, finalRatio ) * getLengthOfOneWheelRotation(sectionWidth, wheelSize) * 60 / 5280
 	}
 }
