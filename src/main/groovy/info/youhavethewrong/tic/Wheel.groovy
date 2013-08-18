4f1439012dc1b2e14aab1c75057e8fd287e0abef
Negated <youhavethewrong@gmail.com>
2013-08-18 11:56:05 -0500

    Added second constructor that takes notes.  Added notes.

--- src/main/groovy/info/youhavethewrong/tic/Wheel.groovy
+++ src/main/groovy/info/youhavethewrong/tic/Wheel.groovy
@@ -5,13 +5,21 @@ public class Wheel {
 	private double wheelDiameter
 	private double nominalSectionWidth
 	private double aspectRatio
-
+	private String notes = ""
 
 	public Wheel(double nominalSectionWidth,
 	double aspectRatio, double wheelDiameter) {
-		this.wheelDiameter = wheelDiameter;
-		this.nominalSectionWidth = nominalSectionWidth;
-		this.aspectRatio = aspectRatio;
+		this.wheelDiameter = wheelDiameter
+		this.nominalSectionWidth = nominalSectionWidth
+		this.aspectRatio = aspectRatio
+	}
+
+	public Wheel(double nominalSectionWidth,
+	double aspectRatio, double wheelDiameter, String notes) {
+		this.wheelDiameter = wheelDiameter
+		this.nominalSectionWidth = nominalSectionWidth
+		this.aspectRatio = aspectRatio
+		this.notes = notes
 	}
 
 	public double getWheelDiameter() {
@@ -36,5 +44,13 @@ public class Wheel {
 
 	public void setAspectRatio(double aspectRatio) {
 		this.aspectRatio = aspectRatio
+	}
+
+	public String getNotes() {
+		notes
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes
 	}
 }
